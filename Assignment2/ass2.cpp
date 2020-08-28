1fa5cbce1e5ff4124095732696ade05e971ff496
zwan074 <42155686+zwan074@users.noreply.github.com>
2020-08-29 09:45:11 +1200

    Add files via upload

--- Assignment2/ass2.cpp
+++ Assignment2/ass2.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
     const int ndata = 50;
     const float xmin = 1.0;
     const float xmax = 100.0;
-
+    double T0,T1,T2,T3;
     MPI::Init(argc, argv);
     int numproc = MPI::COMM_WORLD.Get_size(); // number of buckets
     int myid    = MPI::COMM_WORLD.Get_rank();
@@ -33,15 +33,18 @@ int main(int argc, char *argv[])
     int root = 0;
 
     if (myid == root) {
-        cout << "SEND " << myid << " : "  << endl;
+        T0 = MPI_Wtime();
+        //cout << "SEND " << myid << " : "  << endl;
         for (int i = 0; i < ndata * numproc; ++i) {
             sendbuf_rand_nums[i] = drand48()*(xmax-xmin-1)+xmin;
-            cout <<  sendbuf_rand_nums[i] << ",";
+            //cout <<  sendbuf_rand_nums[i] << ",";
         }   
-        cout << endl;
+        //cout << endl;
+        T0 = MPI_Wtime() - T0;
+        cout << "SEND T0 " << myid << " : "  << T0 << endl;
     }
     
-
+    T1 = MPI_Wtime();
     MPI::COMM_WORLD.Scatter(sendbuf_rand_nums, ndata, MPI_FLOAT, recvbuf_rand_nums, ndata, MPI_FLOAT, root);
 
     vector<vector<float> > small_bucket_2d;
@@ -60,34 +63,36 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < small_bucket_2d.size(); ++i) {
         nitems.push_back(small_bucket_2d[i].size());
-        cout << "SMALL BUCKET RECV " << myid << " : " << " bucket No. " << i << endl;
-        cout << "SMALL BUCKET ITEMS " << myid << " : " << nitems[i] << endl;
+        //cout << "SMALL BUCKET RECV " << myid << " : " << " bucket No. " << i << endl;
+        //cout << "SMALL BUCKET ITEMS " << myid << " : " << nitems[i] << endl;
         for (int j = 0; j < small_bucket_2d[i].size() ; ++j){
             //cout << small_bucket_2d[i][j] << "," ; 
             small_bucket_1d.push_back(small_bucket_2d[i][j]);
         }
-        cout << endl;
+        //cout << endl;
     }
-    cout << "SMALL BUCKET 1D " << myid << " : " << endl;
-    for (int i = 0; i < small_bucket_1d.size() ; ++i){
-        cout << small_bucket_1d[i] << "," ; 
-    }
-    cout << endl;
-
+    //cout << "SMALL BUCKET 1D " << myid << " : " << endl;
+    //for (int i = 0; i < small_bucket_1d.size() ; ++i){
+        //cout << small_bucket_1d[i] << "," ; 
+    //}
+    //cout << endl;
+    //T1 = MPI_Wtime() - T1;
+    //cout << "SMALL BUCKET 1D T1 " << myid << " : " << T1 << endl;
 
 
     MPI_Barrier(MPI_COMM_WORLD);
 
+    //T2 = MPI_Wtime();
     vector<int> recvcnt(numproc);
     MPI::COMM_WORLD.Alltoall(&nitems[0], 1, MPI_INT, &recvcnt[0], 1, MPI_INT);
     vector<int> recvoff(numproc);
     recvoff[0] = 0;
-    cout << "BIG BUCKET OFFSETS " << myid << endl;
+    //cout << "BIG BUCKET OFFSETS " << myid << endl;
     for (int n = 1; n < numproc; ++n) {
         recvoff[n] = recvoff[n-1] + recvcnt[n-1];
-        cout << "BIG BUCKET OFFSETS " << recvoff[n] << ",";
+        //cout << "BIG BUCKET OFFSETS " << recvoff[n] << ",";
     }
-    cout << endl;
+    //cout << endl;
 
     int big_bucket_size = 0 ;
     for (int i = 0; i < recvcnt.size() ; i++) {
@@ -97,13 +102,15 @@ int main(int argc, char *argv[])
     vector<float> big_bucket( big_bucket_size );
     vector<int> sendoff(numproc);
     sendoff[0] = 0;
-    cout << "SMALL BUCKET OFFSETS " << myid << endl;
+    //cout << "SMALL BUCKET OFFSETS " << myid << endl;
     for (int n = 1; n < numproc; ++n) {
         sendoff[n] = sendoff[n-1] + nitems[n-1];
-        cout << "SMALL BUCKET OFFSETS " << sendoff[n] << ",";
+        //cout << "SMALL BUCKET OFFSETS " << sendoff[n] << ",";
     }
-    cout << endl;
-    
+    //cout << endl;
+    //T2 = MPI_Wtime() - T2;
+    //cout << "SMALL BUCKET OFFSETS T2 " << myid << " : "  << T2 << endl;
+
     MPI_Barrier(MPI_COMM_WORLD);
     MPI::COMM_WORLD.Alltoallv(
         &small_bucket_1d[0], &nitems[0], &sendoff[0], MPI_FLOAT,
@@ -111,12 +118,12 @@ int main(int argc, char *argv[])
     
     qsort (&big_bucket[0], big_bucket.size(), sizeof(float), compare);
 
-    cout << "BIG BUCKET No. " << myid << endl;
+    //cout << "BIG BUCKET No. " << myid << endl;
     
-    for (int i = 0; i < big_bucket.size() ; i++) {
-        cout << big_bucket[i] << "," ;
-    }
-    cout << endl;
+    //for (int i = 0; i < big_bucket.size() ; i++) {
+        //cout << big_bucket[i] << "," ;
+    //}
+    //cout << endl;
 
     MPI_Barrier(MPI_COMM_WORLD);
 
@@ -125,7 +132,7 @@ int main(int argc, char *argv[])
     vector<int> recvcnt_final(numproc);
     sendcnt_final[0] = big_bucket.size();
 
-    MPI::COMM_WORLD.Gather( &sendcnt_final[0], 1, MPI_INT, &recvcnt_final[0], 1, MPI_INT, 0);
+    MPI::COMM_WORLD.Gather( &sendcnt_final[0], 1, MPI_INT, &recvcnt_final[0], 1, MPI_INT, root);
 
     vector<int> recvcnt_final_off(numproc);
     recvcnt_final_off[0] = 0;
@@ -135,8 +142,12 @@ int main(int argc, char *argv[])
 
     MPI::COMM_WORLD.Gatherv( &big_bucket[0], big_bucket.size(), MPI_FLOAT, 
                             &final_sorted_vector[0], &recvcnt_final[0], &recvcnt_final_off[0] , MPI_FLOAT, 0);
-    if (myid == 0) {
-        cout << " final vector : " << endl;
+
+    T1 = MPI_Wtime() - T1;
+    cout << "T1 " << myid << " : " << T1 << endl;
+    
+    if (myid == root) {
+        cout << " final vector : " << final_sorted_vector.size() << endl;
         for (int i = 0; i < final_sorted_vector.size() ; i++) {
             cout << final_sorted_vector[i] << "," ;
         }
