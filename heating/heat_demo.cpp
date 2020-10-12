75ec921b7a20f092e199a5f0a7f5a4cfbf3efe55
zwan074 <42155686+zwan074@users.noreply.github.com>
2020-10-12 16:28:51 +1300

    Add files via upload

--- heating/heat_demo.cpp
+++ heating/heat_demo.cpp
@@ -1,18 +1,10 @@
 /******************************************************************************
-
 Start up demo program for 159735 Assignment 3 Semester 1 2013
-
 All this does is initialize the image and write it to a file.
-
 To compile:
-
 make heat_demo
-
 To run (for example to make a 100X100 pixel image):
-
 ./heat_demo 100
-
-
 ******************************************************************************/
 #include <iostream>
 #include <string>
@@ -22,7 +14,56 @@ To run (for example to make a 100X100 pixel image):
 #include "arrayff.hxx"
 #include "draw.hxx"
 
-#define NUM_THREADS 5
+#define NUM_THREADS 1
+
+int heat_seq(int argc, char* argv[]) 
+{
+  const float tol = 0.00001;
+  const int npix = atoi(argv[1]);
+  const int npixx = npix;
+  const int npixy = npix;
+  const int ntotal = npixx * npixy;
+ 
+  Array<float, 2> h(npixy, npixy), g(npixy, npixx);
+
+  const int nrequired = npixx * npixy;
+  const int ITMAX = 1000000;
+
+  int iter = 0;
+  int nconverged = 0;
+
+  fix_boundaries2(h);
+
+  double T0, T1;
+  T0 = omp_get_wtime();
+
+
+  do {
+
+    for (int y = 1; y < npixy-1; ++y) {
+      for (int x = 1; x < npixx-1; ++x) {
+	      g(y, x) = 0.25 * (h(y, x-1) + h(y, x+1) + h(y-1, x) + h(y+1,x));
+      }
+    }
+
+    fix_boundaries2(g);
+
+    nconverged = 0;
+    for (int y = 0; y < npixy; ++y) {
+      for (int x = 0; x < npixx; ++x) {
+        float dhg = std::fabs(g(y, x) - h(y, x));
+        if (dhg < tol) ++nconverged;
+        h(y, x) = g(y, x);
+      }
+    }
+    ++iter;
+
+  } while (nconverged < nrequired && iter < ITMAX);
+  T1 = omp_get_wtime();
+  
+  std::cout << "Seq Required " << iter << " iterations" << std::endl;
+  std::cout << "Seq Required " << T1-T0 << " time" << std::endl;
+}
 
 int sum_vector(std::vector<int> vector) {
   int ret = 0 ;
